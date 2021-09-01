2ec7ff7121a40a667ecb68f3ce49891555ba7e20
danielnlz01 <38385574+danielnlz01@users.noreply.github.com>
2021-08-31 21:03:34 -0500

    Add files via upload

---
+++ main(25).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void selectionSort(int a[], int len);
+
+int main() 
+{
+	int d[10] = {32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
+	
+// i=0, i=5 minimo es 14
+// i=1, i=3 minimo es 18
+// i=2, i=3 minimo es 27
+
+//checar el numero menor y reemplazar el valor, debe ser void
+  return 0;
+}
+
+void selectionSort(int a[], int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		
+	}
+}
